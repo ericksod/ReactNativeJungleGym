f697409e3bf0c35cd7718358f84efbd772e5c91e
Danielle Erickson <e.erickson.d@gmail.com>
2016-02-11 17:31:15 -0500

    Clean up index.android.js

--- index.android.js
+++ index.android.js
@@ -28,65 +28,17 @@ constructor(props) {
       dataSource: new ListView.DataSource({
         rowHasChanged: (row1, row2) => row1 !== row2,
       }),
-      loaded: false,
     };
   }
 
-  componentDidMount() {
-    this.fetchData();
-  }
-
-  fetchData() {
-    fetch(REQUEST_URL)
-      .then((response) => response.json())
-      .then((responseData) => {
-        this.setState({
-          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
-          loaded: true,
-        });
-      })
-      .done();
-  }
-
   render() {
-    if (!this.state.loaded) {
-      return this.renderLoadingView();
-    }
-
     return (
       <MyCustomView style={styles.container}>
       </MyCustomView>
     );
   }
-
-  renderLoadingView() {
-    return (
-      <View style={styles.container}>
-        <Text>
-          Loading movies...
-        </Text>
-      </View>
-    );
-  }
-
-  renderMovie(movie) {
-    return (
-      <View style={styles.container}>
-      <MyCustomView></MyCustomView>
-      <Text>
-          THIS IS A MOVIE
-        </Text>
-        <Image
-          source={{uri: movie.posters.thumbnail}}
-          style={styles.thumbnail}/>
-        <View style={styles.rightContainer}>
-          <Text style={styles.title}>{movie.title}</Text>
-          <Text style={styles.year}>{movie.year}</Text>
-        </View>
-      </View>
-    );
-  }
 }
+  
 
 var styles = StyleSheet.create({
   container: {
@@ -96,31 +48,7 @@ var styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#50f0dc',
   },
-  thumbnail: {
-    width: 53,
-    height: 81,
-  },
-  rightContainer: {
-    flex: 1,
-  },
-  title: {
-    fontSize: 20,
-    marginBottom: 8,
-    textAlign: 'center',
-  },
-  year: {
-    textAlign: 'center',
-  },
-  listView: {
-    paddingTop: 20,
-    backgroundColor: '#F5FCFF',
-  },
 });
-
-
-var MOCKED_MOVIES_DATA = [
-  {title: 'Star Wars', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
-];
 
 
 AppRegistry.registerComponent('ReactNativeTutorial', () => ReactNativeTutorial);
